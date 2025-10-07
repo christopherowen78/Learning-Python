42a7cd80bf3b8980ab5dd92cfd17113f49e6fbbf
Christopher Owen <christopher.owen@bbc.co.uk>
2025-10-07 17:01:38 +0100

    Chapter 1

---
+++ 1_Getting_Started.py
@@ -0,0 +1,17 @@
+print('Hello World!')
+
+var = 8
+print(var)
+
+var = 3.142
+print(var)
+
+var = 'Random Text'
+print(var)
+
+var = True
+print(var)
+
+# Initialise program status
+running = True
+print('Run state: ', running)
